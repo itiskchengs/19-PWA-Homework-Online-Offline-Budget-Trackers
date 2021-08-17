d67b53abf1a514232f2fc068540dff43207cb571
Kenneth Cheng <itskcheng@gmail.com>
2021-08-17 19:46:56 -0400

    New update

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.static("public"));
 // });
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/progressive-web-app", {
+  process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
